18d2b4c66c454b6123e3d3359f8506236eba6a97
kaptsov <kaptsovaa@gmail.com>
2018-02-08 11:19:45 +0900

    Первая попытка сдать

--- bars.py
+++ bars.py
@@ -1,21 +1,65 @@
 import json
+from geopy.distance import vincenty
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf-8') as json_file:
+        return json.loads(json_file.read())
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(bar_list):
+    biggest_bar = max(
+        bar_list,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+    )
+    return biggest_bar
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(bar_list):
+    smallest_bar = min(
+        bar_list,
+        key=lambda bar: bar['properties']['Attributes']['SeatsCount']
+    )
+    return smallest_bar
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(bar_list, user_location):
+    closest_bar = min(
+        bar_list,
+        key=lambda bar: vincenty(get_bar_point(bar), user_location).m
+    )
+    return closest_bar
+
+
+def get_bar_list(json_data):
+    return json_data['features']
+
+
+def get_bar_point(bar):
+    longitude, latitude = bar['geometry']['coordinates']
+    bar_point = (longitude, latitude)
+    return bar_point
+
+
+def get_bar_name(bar):
+    return bar['properties']['Attributes']['Name']
 
 
 if __name__ == '__main__':
-    pass
+    json_data = load_data('bars.json')
+    bar_list = get_bar_list(json_data)
+    try:
+        user_longitude, user_latitude = map(
+                                            float,
+                                            input('Input longitude, latitude:')
+                                            .split()
+                                            )
+    except ValueError:
+        exit('Need to enter two float values ')
+    user_location = (user_longitude, user_latitude)
+    print('The biggest bar is', get_bar_name(get_biggest_bar(bar_list)))
+    print('The smallest bar is', get_bar_name(get_smallest_bar(bar_list)))
+    print('The closest bar is', get_bar_name(get_closest_bar(
+                                                             bar_list,
+                                                             user_location
+                                                             )))
